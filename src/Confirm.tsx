41205336f0ac7dc0f18919e2d346d5de607dd319
SpikeHD <spikegdofficial@gmail.com>
2022-12-25 01:49:45 -0800

    email confirmation section

---
+++ src/Confirm.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'preact/hooks'
+import './Confirm.css'
+
+export function Confirm() {
+  const [confirmed, setConfirmed] = useState(false)
+  const params = new URLSearchParams(window.location.search)
+  let err = false
+
+  if (!confirmed) {
+    fetch(`${process.env.API_URL}/confirm`, {
+      method: "POST",
+      body: JSON.stringify({
+        code: params.get('code'),
+      })
+    }).then(() => {
+      setConfirmed(true)
+    }).catch((e) => {
+      console.error(e)
+    })
+  }
+  
+  return (
+    <div class="confirm_main">
+      <div class="confirm_header">Confirming your email...</div>
+      <div class="confirm_message">
+        {
+          confirmed ? (
+            <>Email confirmed! Head back on over to <a href="/login">the login page</a> to log in!</>
+          ) : 'We are just confirming your email, one moment!'
+        }
+
+        {
+          err ? 'An error occurred...' : ''
+        }
+      </div>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,26 @@ import { render } from 'preact'
 import { App } from './app'
 import { Login } from './Login'
 import './index.css'
+import { Confirm } from './Confirm'
 
 // TODO change
 process.env.API_URL = 'http://localhost:4000'
 
-if (window.localStorage.getItem('auth_key')) {
-  // @ts-expect-error shut up
-  render(<App />, document.getElementById('app'))
-} else {
-  // @ts-expect-error shut up
-  render(<Login />, document.getElementById('app'))
-}
+;(() => {
+  // Email confirmation, special case
+  if (window.location.pathname === '/confirm') {
+    // @ts-expect-error shut up
+    render(<Confirm />, document.getElementById('app'))
+    return
+  }
+
+  if (window.localStorage.getItem('auth_key')) {
+    // @ts-expect-error shut up
+    render(<App />, document.getElementById('app'))
+  } else {
+    // @ts-expect-error shut up
+    render(<Login />, document.getElementById('app'))
+  }
+})()
+
 
